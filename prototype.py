3fe7ec0ce23c571129ad84c394fa27e8ee5c8a71
DavidBoern <davidboern@hotmail.com>
2025-08-17 18:42:58 +0200

    Programm funktioniert nicht mehr!

--- prototype.py
+++ prototype.py
@@ -51,7 +51,7 @@ class Textvorverarbeiter:
         textListe = self._zahlenNormalisieren(textListe)
         textListe = self._abkuerzungenNormalisieren(textListe)
         textListe = self._waehrungszeichenNormalisieren(textListe)
-        textListe = self.punktuationEntfernen(textListe)
+        textListe = self._punktuationEntfernen(textListe)
         return (textListe)
 
     def _lowercase(self,t):
@@ -115,11 +115,11 @@ class Textvorverarbeiter:
     def _abkuerzungenNormalisieren(self,wortListe):
         return [self._tokenMitAbkuerzungsMapVergleichen(t) for t in wortListe]
     
-# in der folgenden Methode bleiben Elemente, die lediglich aus Satzzeichen bestehen, als leere Elemente in der Liste. Sie lässt somit (wie sämtliche Vorverarbeitungsschritte) die Länge der Wortliste (und auch die einzelnen Indexpositionen) unverändert. Das erleichtert die später vorgesehene Rückabwicklung der Vorverarbeitungsschritte: 
-    def punktuationEntfernen (self, wortListe):
+# In der folgenden Methode bleiben Elemente, die lediglich aus Satzzeichen bestehen, als leere Elemente in der List. Dadurch bleibt die Länge der Wortliste (und auch die einzelnen Indexpositionen) unverändert. Das erleichtert die später vorgesehene Rückabwicklung der Vorverarbeitungsschritte. 
+    def _punktuationEntfernen (self, wortListe):
         return [t.strip(string.punctuation.replace("|", "")) for t in wortListe]
               
-# Klasse Levenshtein erbt in der textdistance-Library von "__Base". Diese Funktionalität isthier in der Klasse Levenshtein integriert
+# Klasse Levenshtein erbt in der textdistance-Library von "__Base". Diese Funktionalität ist hier in der Klasse Levenshtein integriert.
 class Levenshtein:
 
 #ursprünglich aus Base geerbt:
@@ -160,8 +160,9 @@ class Levenshtein:
                 edit = prev[c - 1] + (not dist)
                 cur[c] = min(edit, deletion, insertion)
         return int(cur[-1])
-      
+    # Die folgende Methode orientiert sich an der oben angegebenen Methode "_cycled" aus der "textdistance"-Library. Im Unterschied zur "_cycled"-Methode ist es hierbei allerdings nicht der Ähnlichkeitswert zwischen zwei Textdokumenten von Belang. Stattdessen liefert die Methode die entsprechende Matrix zur Berechnung dieses Wertes zurück. Auf Grundlage dieser Matrix lassen sich im folgenden die einzelnen Editierschritte identifizieren.       
     def berechneTransformationsmatrix (self, s1: Sequence[T], s2: Sequence[T], kostenfunktion: dict[tuple[T, T], float]):
+        STANDARD_ERSETZUNGSKOSTEN = 3   # dieser Wert wird hoch angelegt, da Ersetzungen von Begriffen, die sich in den zu vergleichenden Textdateien in unterschiedlichen Bereichen berfinden, eher vermieden werden sollten. Im nächsten Meilenstein werden die Auswirkungen unterschiedlich festgelegter Werte untersucht.
         rows = len([c for c in s1 if c != "|"]) + 1
         cols = len(s2) + 1 
         prev = None
@@ -176,47 +177,44 @@ class Levenshtein:
         while j<rows:
             transformationsprotokoll [j][0] = "d"
             j=j+1 
-        r_matrix = 1
-        for r_sequenz in range(len(s1)):
-            if s1[r_sequenz] == "|": 
+        rMatrix = 1
+        for rSequenz in range(len(s1)):
+            if s1[rSequenz] == "|": 
                 continue          
-            prev, cur = cur, [r_matrix] + [0] * (cols - 1)
+            prev, cur = cur, [rMatrix] + [0] * (cols - 1)
             for c in range(1, cols):              
                 deletion = prev[c] + 1
                 insertion = cur[c - 1] + 1
-                gleich = self.test_func(s1[r_sequenz], s2[c - 1])
+                gleich = self.test_func(s1[rSequenz], s2[c - 1])
                 if gleich: ersetzKosten = 0
                 else: 
-                    ersetzKosten = kostenfunktion.get((s1[r_sequenz], s2[c - 1]), 3)
+                    ersetzKosten = kostenfunktion.get((s1[rSequenz], s2[c - 1]), STANDARD_ERSETZUNGSKOSTEN)
                 edit = prev[c - 1] + ersetzKosten
                 cur[c] = min(edit, deletion, insertion)
                 if cur[c] == edit:
-                    transformationsprotokoll[r_matrix][c] = "e"
+                    transformationsprotokoll[rMatrix][c] = "e"
                 elif cur[c] == deletion:
-                    transformationsprotokoll[r_matrix][c] = "d"
+                    transformationsprotokoll[rMatrix][c] = "d"
                 else:
-                    transformationsprotokoll[r_matrix][c] = "i"
-            r_matrix +=1
+                    transformationsprotokoll[rMatrix][c] = "i"
+            rMatrix +=1
         return transformationsprotokoll
 
 class Tokenuebertragung:
     def uebertrageToken(self, s1: Sequence[T], s2: Sequence[T], transformationsprotokoll: list[list[str]]):
         row_sequenz_anfangsindex = len(s1)-1
-        #print (f"row_sequenz_anfangsindex = {len(s1)-1}")
-        row_matrix_anfangsindex = len([c for c in s1 if c != "|"]) # List comprehension
-        col_anfangsindex = len(s2)  # Matrixindex geht bis len(s2), Index von s2 nur bis len(s2)-1
+        row_matrix_anfangsindex = len([c for c in s1 if c != "|"])
+        col_anfangsindex = len(s2)
         self._syncSchritteAusfuehren (s1, s2, transformationsprotokoll, row_sequenz_anfangsindex, row_matrix_anfangsindex, col_anfangsindex)
         return s1
-        #zum Test:
-        #print (s1)      
-
+        
     def _syncSchritteAusfuehren (self, asr: Sequence[T], mt: Sequence[T], transformationsprotokoll: list[list[str]], row_sequenz_index, row_matrix_index, col_index):
         while row_sequenz_index >= 0:
             if asr[row_sequenz_index]=="|": row_sequenz_index -=1
-            match transformationsprotokoll[row_matrix_index][col_index]:           
+            # die einzelnen Editierschritte lassen sich identifizieren, indem die Transformationsmatrix vom Ende bis zum Anfang durchlaufen wird. Da es unter Umständen meherere Transfarmationspfade gibt, mit denen bei minimalen Editierschritten die Übertragung der Dateien erfolgen kann, wird -bei gleichen Kosten der entsprechenden Editierschritte - folgende Priorisierung vorgenommen. 1. Ersetzung, 2. Löschung, 3. Einfügung.   
+            match transformationsprotokoll[row_matrix_index][col_index]:                        
                 case "e":
-                    asr[row_sequenz_index] = mt [col_index-1] # Wenn Wort z.B. 4 Zeichen lang, dann ist die letzte Stelle Index =3                  
-                    #print (f"e wird aufgerufen.Die Stelle {row_sequenz_index} wird durch {s2[col_index-1]} ersetzt.")
+                    asr[row_sequenz_index] = mt [col_index-1] 
                     row_sequenz_index -= 1
                     row_matrix_index -= 1
                     col_index -= 1                    
@@ -224,60 +222,48 @@ class Tokenuebertragung:
                     asr.pop(row_sequenz_index)
                     row_sequenz_index -= 1
                     row_matrix_index -= 1
-                    #print (f"d wird aufgerufen.{row_sequenz_index} wird entfernt.")
                 case "i":
                     asr.insert(row_sequenz_index+1, mt[col_index-1])
-                    #print (f"i wird aufgerufen. An der Stelle {row_sequenz_index+1} wird {s2[col_index-1]} eingefügt, eins mehr wäre ein {s2[col_index]} ")
                     col_index -=1
-        #if row_sequenz == 0 or row_matrix == 0 or col == 0:
-#            self.syncSchrittAusfuehren(s1, s2, transformationsprotokoll, row_sequenz_index, row_matrix_index, col_index)
         else:
             return asr
     
-    def _zeilenumbruecheProtokollieren (self,asr):
-        umbruchstellen = []
+    def asrZeilenumbruecheProtokollieren (self,asr):
+        asrUmbruchstellen = []
         for i in range(len(asr)):
             if asr[i] == "|":
-                umbruchstellen.append (i)
-        return umbruchstellen
+                asrUmbruchstellen.append (i)
+        return asrUmbruchstellen
   
 class Kostenfunktion():
     def __init__(self):
         self.aehnlichkeitsmass = {}
-
+    
+    # die betrachteten Ersetzungskosten werden auf 1 normiert: 
     def _normiereKostenFunktion(self, dict_Werte):
         faktor = len(dict_Werte)/sum(dict_Werte.values())
         for key in dict_Werte:
             dict_Werte[key]=dict_Werte[key]*faktor
         return dict_Werte       
-    
-    # Um den Rechenaufwand zu minimieren, werden zur Integration gewichteter Ersetzungskosten lediglich Tokenpaare betrachtet, die sich in benachbarten Bereichen der beiden Tokensequenzen befinden. Zudem lässt sich so realisieren, dass "weit entfernte" Ersetzungen mit hohen Kosten verbunden sind. 
-    # alte Methode 
-    # def _erstelleTokenpaare(self, token_List1, token_List2):
-    #     eindeutigeToken = sorted(set(token_List1 + token_List2))
-    #     tokenKombinationen = itertools.combinations(eindeutigeToken,2)
-    #     return tokenKombinationen                              
-
               
     def _erstelleTokenpaare(self, gesamtListe1, gesamtListe2):
-        MINGROESSE_TESTSET1 = 40
+        MINGROESSE_TESTSET1 = 40   # abweichende Setgrößen werden im Rahmen der Evaltuation erprobt.
         anzahlTestSets = int(len(gesamtListe1)/MINGROESSE_TESTSET1)
-        print (f"anzahlTestSets= {anzahlTestSets}")
-        print (f"len(gesamtListe1) = {len(gesamtListe1)}")
         groesseSet1 = float(len(gesamtListe1) / anzahlTestSets)
         groesseSet2 = float(len(gesamtListe2) / anzahlTestSets)
-        print (f"groesseSet1 + groesseSet2 = {groesseSet1}, {groesseSet2}")
-        # um harte Setgrenzen zu verhindern wird überlappend iteriert.
+        # um harte Setgrenzen zu verhindern, wird überlappend iteriert.
         testSets =[]
-        for s in testSets:
-        for kombination in itertools.combinations(s, 2):
-            # 1. Nur aufnehmen, wenn kein "|" enthalten ist
-            if "|" not in kombination[0] and "|" not in kombination[1]:
-                # 2. Normal und gespiegelte Reihenfolge aufnehmen
-                tokenKombinationen.add(kombination)
-                tokenKombinationen.add((kombination[1], kombination[0]))
+        for i in range (2*anzahlTestSets):
+            testSets.append (gesamtListe1[int(0.5*i*groesseSet1):int((0.5*i+1)*groesseSet1)] + gesamtListe2[int(0.5*i*groesseSet2):int((0.5*i+1)*groesseSet2)])
+        for i in range(len(testSets)):
+            testSets[i]=sorted (set(testSets[i]))
+        tokenKombinationen = set ()
+        for s in testSets:          
+            for kombination in itertools.combinations(s, 2):
+                if "|" not in kombination[0] and "|" not in kombination[1]:
+                    tokenKombinationen.add(kombination)
+                    tokenKombinationen.add((kombination[1], kombination[0]))
         tokenKombinationen = tuple (tokenKombinationen)
-        print (f"Hier die TokenKombinationen: {tokenKombinationen}")
         return tokenKombinationen               
 
     def berechneTokenDistanzen(self, gesamtListe1, gesamtListe2):
@@ -286,23 +272,17 @@ class Kostenfunktion():
         for tupel in tokenKombinationen:
             self.aehnlichkeitsmass.update({tupel:(levenshtein._cycled(tupel[0], tupel[1]))/(max(len(tupel[0]),len(tupel[1])))})        
         self.aehnlichkeitsmass = self._normiereKostenFunktion(self.aehnlichkeitsmass)
-        #zum Test:
-        #print (dict(sorted(self.aehnlichkeitsmass.items(), key=lambda item: item[1])))
-        #print (self.aehnlichkeitsmass)
-        return self.aehnlichkeitsmass
 
 class ASRKorrektur:
     def _zeilentextErstellen(self, umbrueche, wortliste, asr):
         teile, start = [], 0
+        # die im ASR-Extrakt eingefügten Zeilenmarker verschieben die Positionen der Zeilenumbrüche. Dieser Umstand findet hier Berücksichtigung.
         umbruecheModifiziert = []
         for u in umbrueche:
             umbruecheModifiziert.append (u-(umbrueche.index(u)+1))
-        print (umbruecheModifiziert)
-        for stop in umbruecheModifiziert: # + [len(wortliste)-1]:
+        for stop in umbruecheModifiziert:
             teile.append(" ".join(wortliste[start:stop+1]))
             start = stop + 1
-        print (f"teile = {teile}")
-        print (f"ASR = {asr}")        
         for i in range(len(teile)):
             asr.iat[i, 2] = teile[i]        
         asr.to_csv("output.csv", sep="\t", index=False)
@@ -327,32 +307,19 @@ def main():
 
     tv = Textvorverarbeiter()
 
-    #print (f"Textliste des ASR-Extrakts OHNE Vorverarbeitung:\n {asr.wortListeMitMarkern} \nLänge: {len(asr.wortListeMitMarkern)}")
     asrVergleichsText = tv.vorverarbeite (asr.wortListeMitMarkern)
-    #print (f"Textliste des  ASR-Extrakts MIT Vorverarbeitung:\n {asrVergleichsText} \nLänge: {len(asrVergleichsText)}")
-
-    #print (f"Textliste des manuellen Transkripts OHNE Vorverarbeitung:\n {mt.wortListeOhnePraefixe} \nLänge: {len(mt.wortListeOhnePraefixe)}")
     mtVergleichsText = tv.vorverarbeite (mt.wortListeOhnePraefixe)
-    #print (f"Textliste des manuellen Transkripts MIT Vorverarbeitung:\n {mtVergleichsText} \nLänge: {len(mtVergleichsText)}")
 
-    # Kostenfunktion erstellen (evtl. nicht sinnvoll: rechenintensiv und geringer Mehrwert. Erstmal weglassen ):
     k = Kostenfunktion()
     aehnlichkeitsmass = k.berechneTokenDistanzen (mtVergleichsText, asrVergleichsText)
-    # #zum Test
-    #print(f"Hier die Kostenfunktion:{k.aehnlichkeitsmass}")
 
-    # Transformationsmatrix berechnen:
     l = Levenshtein()
     transformationsmatrix = l.berechneTransformationsmatrix(asrVergleichsText, mtVergleichsText, aehnlichkeitsmass) 
 
     t = Tokenuebertragung ()
     korrigiertesASR = t.uebertrageToken(asrVergleichsText, mtVergleichsText, transformationsmatrix)
-    # zum Test:
-    print(f"Hier zum Test das korrigierte ASR, allerdings noch mit Pipe statt Zeilenumbruch und durch Vorverarbeitungssschritte modifiziert: {korrigiertesASR}")
 
-    # zum Test:
-    zeilenumbrueche = t._zeilenumbruecheProtokollieren(korrigiertesASR)
-    print (f"Stellen mit Zeilenumbrüchen: {zeilenumbrueche}")
+    zeilenumbrueche = t.asrZeilenumbruecheProtokollieren(korrigiertesASR)
 
     csv = ASRKorrektur()
     csv._zeilentextErstellen (zeilenumbrueche, mt.originalWortListeOhnePraefixe, asr.df)
